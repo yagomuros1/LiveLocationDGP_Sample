eb11c0cf1f00b50b4d48cd0975a8ece6d3aef5d9
Yago <yrey@cleventy.com>
2020-05-05 20:45:07 +0200

    Update sample

---
+++ app/src/main/java/com/newconcepts/MapsActivity.kt
@@ -0,0 +1,72 @@
+package com.newconcepts
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.Observer
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    companion object {
+        private const val ZOOM = 18f
+    }
+
+    private var mMap: GoogleMap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun getLocationUpdates(map: GoogleMap?) {
+        CurrentLocationListener.getInstance(applicationContext).observe(this, Observer { location ->
+            if (location != null) {
+
+                mMap?.clear()
+
+                val currentLocation = LatLng(location.latitude, location.longitude)
+                // create marker
+                val marker = MarkerOptions().position(currentLocation).title("Hello Maps");
+
+                // adding marker
+                mMap?.addMarker(marker)
+
+                mMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, ZOOM))
+            }
+        })
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1000) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                mMap?.let { getLocationUpdates(it) }
+
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), 1000)
+        } else {
+            getLocationUpdates(mMap)
+        }
+    }
+
+}
